9bc3045a3bc7cf68c1711cd0112bc99c76f72ca4
Nishir <nshelat@usc.edu>
2017-03-21 13:34:48 -0700

    Added projects page

--- script.js
+++ script.js
@@ -1,22 +1,3 @@
-$( "#aboutButton" ).click(function() {
-  $('html,body').animate({
-        scrollTop: $("#about").offset().top},
-        'slow');
-});
-
-$( "#eboardButton" ).click(function() {
-  $('html,body').animate({
-        scrollTop: $("#eboard").offset().top},
-        'slow');
-});
-
-$( "#homeButton" ).click(function() {
-  $('html,body').animate({
-        scrollTop: $("body").offset().top},
-        'slow');
-});
-
-
 var didScroll;
 var lastScrollTop = 0;
 var delta = 5;
